94479a00a80f96aaf4a6c310693706fa8e987c77
mika <99153848+mknkgwr1005@users.noreply.github.com>
2024-06-19 10:49:40 +0900

    Revert "Revert 7 add feature/quick announcement"

---
+++ src/components/ButtomBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    <div>フッター</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// defineExpose({
+//   // メソッド
+// });
+</script>
+<style scoped>
+.footer {
+  height: 300px;
+  background-color: #e3f2fd;
+}
+</style>
--- src/components/QuickAnnouncement.vue
+++ src/components/QuickAnnouncement.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="d-flex flex-wrap align-content-center justify-content-center">
     <div class="card">
-      <div class="card-body">速報</div>
+      <div class="card-body">
+        <div v-if="store.state.announceData">
+          <b-card
+            v-for="product of store.state.announceData"
+            :key="product?.index"
+            :title="product.name"
+          >
+            <b-card-text>
+              <b-button @click="goToUrl(product.url)">購入する</b-button>
+            </b-card-text>
+          </b-card>
+        </div>
+      </div>
+      <div class="errorMessage" :v-if="store.state.announceData.length === 0">
+        入荷情報はまだありません
+      </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { defineComponent } from "vue";
-export default defineComponent({
-  setup() {
-    return {};
-  },
-});
+import store from "../store/index";
+import { onMounted } from "@vue/runtime-core";
+
+// 外部URLに遷移する
+const goToUrl = (url: string) => {
+  window.location.href = url;
+};
 </script>
+
+<style scoped>
+.errorMessage {
+  width: auto;
+  height: auto;
+}
+</style>
